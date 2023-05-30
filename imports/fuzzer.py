56d6ebb15ff91822515fe22e3e38c5d2b38b1921
xStFtx <you@example.com>
2023-05-30 06:35:20 +0200

    Modular

---
+++ imports/fuzzer.py
@@ -0,0 +1,46 @@
+import subprocess
+
+class Fuzzer:
+    def __init__(self, target_url, wordlist_path):
+        self.target_url = target_url
+        self.wordlist_path = wordlist_path
+
+    def fuzz_directories(self):
+        cmd = [
+            'ffuf',
+            '-u', f'{self.target_url}/FUZZ',
+            '-w', self.wordlist_path,
+            '-t', '50',
+            '-mc', '200',
+            '-fs', '0'
+        ]
+
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        stdout, stderr = process.communicate()
+
+        if process.returncode == 0:
+            print(stdout)
+        else:
+            print(f'Error occurred while fuzzing directories: {stderr}')
+
+    def fuzz_files(self):
+        extensions = ['.php', '.html', '.txt']  # Add more file extensions as needed
+
+        for extension in extensions:
+            cmd = [
+                'ffuf',
+                '-u', f'{self.target_url}/FUZZ{extension}',
+                '-w', self.wordlist_path,
+                '-t', '50',
+                '-mc', '200',
+                '-fs', '0'
+            ]
+
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            stdout, stderr = process.communicate()
+
+            if process.returncode == 0:
+                print(stdout)
+            else:
+                print(f'Error occurred while fuzzing files with extension {extension}: {stderr}')
+
